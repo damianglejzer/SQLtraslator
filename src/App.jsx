a07162f1457a4393ddaf15e9d34363c8a8e5810c
damianglejzer <118016187+damianglejzer@users.noreply.github.com>
2025-02-15 16:43:36 +0100

    Update App.jsx
    color of the button

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
           />
           <button className={styles.mybutton}>
             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"
-                  viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round"
+                  viewBox="0 0 24 24" strokeWidth="2" stroke="#212121" fill="none" strokeLinecap="round"
                   strokeLinejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                   <path d="M5 12l14 0"></path>
